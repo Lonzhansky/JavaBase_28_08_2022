9003da4095ff71365ed40e98f94ccbbb2c8cd80a
Lonzhansky Denis <lonzhansky@gmail.com>
2022-09-28 21:32:04 +0300

    lesson 10

---
+++ src/main/java/com/hillel/loznhansky/lessons/lesson10/homeworks/Homework11.java
@@ -0,0 +1,19 @@
+package com.hillel.loznhansky.lessons.lesson10.homeworks;
+
+import java.util.Scanner;
+
+public class Homework11 {
+    public static void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
+//        int n = getNumber(scanner);
+//        int m = getNumber(scanner);
+//
+//
+//        int[][] array1 = new int[n][m];
+//        int[][] array2 = new int[m][n];
+
+
+
+    }
+}
